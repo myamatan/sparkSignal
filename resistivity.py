40ac5a3499e34f01ea5f34ecb9e53abafee5c19e
Masahiro Yamatani <masahiro.yamatani@cern.ch>
2018-10-07 22:52:58 +0200

    Add correlation calcu

--- resistivity.py
+++ resistivity.py
@@ -1,13 +1,17 @@
 import numpy as np
 import pandas as pd
+import scipy as sp
+from scipy.stats import pearsonr
+import statsmodels.api as sm
+from statsmodels.stats.outliers_influence import summary_table
 
 import matplotlib.pylab as plt
 
 CLF = 1.96
 xlabel='Surface'
 #ylabel='HVline'
-#ylabel='count630'
-ylabel='amp630'
+ylabel='count630'
+#ylabel='amp630'
 
 yerrlabel='HVlineStd'
 if ylabel=='count630':
@@ -24,8 +28,24 @@ df['countErr'] = np.sqrt(df['count630']) * CLF
 df['ampErr630'] = df['ampStd630']/np.sqrt(df['count630']) * CLF
 print(df)
 
+# Correlation
+r, p = pearsonr(df[xlabel],df[ylabel])
+
+
+# Fit
+X = sm.add_constant(df[xlabel])
+res = sm.OLS(df[ylabel], X).fit()
+st, data, ss2 = summary_table(res, alpha=0.05)
+fittedvalues = data[:,2]
+predict_mean_se  = data[:,3]
+predict_mean_ci_low, predict_mean_ci_upp = data[:,4:6].T
+predict_ci_low, predict_ci_upp = data[:,6:8].T
+
 # Plot
 df.plot.scatter(x=xlabel,y=ylabel,xerr=df[['SurfaceStd']].values.T, yerr=df[[yerrlabel]].values.T, grid=True,color='red')
+plt.plot(X, fittedvalues, 'b--', linewidth=1)
+plt.plot(X, predict_mean_ci_low, 'g--', linewidth=1)
+plt.plot(X, predict_mean_ci_upp, 'g--', linewidth=1)
 
 # plot style
 # +++++++++++++++++++++++++++++++++++++++
@@ -41,9 +61,15 @@ yRange=yMax-yMin
 
 plt.text(xMin+xRange*0.05,
          yMax-yRange*0.165,
-         #'Sensitive area resistivity [MOhm/sq]\nv.s.\nSensitive area to HV line resistivity [MOhm]',
+         'Sensitive area resistivity [MOhm/sq]\nv.s.\nSensitive area to HV line resistivity [MOhm]',
          #'Sensitive area resistivity [MOhm/sq]\nv.s.\nSpark count',
-         'Sensitive area resistivity [MOhm/sq]\nv.s.\nMean shape amplitude [V]',
+         #'Sensitive area resistivity [MOhm/sq]\nv.s.\nMean shape amplitude [V]',
+         size=11,
+         bbox=dict(facecolor='white',alpha=0.8))
+
+plt.text(xMin+xRange*0.6,
+         yMax-yRange*0.925,
+         'Correlation r: {r}'.format(r=round(r,3))+'\nSignificance p: {p}'.format(p=round(p,3)),
          size=11,
          bbox=dict(facecolor='white',alpha=0.8))
 
@@ -56,7 +82,7 @@ plt.xlabel('Sensitive area resistivity [MOhm/sq]')
 plt.ylabel('Sensitive are to HV line resistivity [MOhm]')
 if ylabel=='count630':
     plt.ylabel('Spark count')
-elif ylabel=='amp360':
+elif ylabel=='amp630':
     plt.ylabel('Mean Shape Amplitude [V]')
 
 plt.xlim(xMin, xMax)
